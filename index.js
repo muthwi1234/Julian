afc982f808814aa8b47153c7ce1a3014a9b029ca
peter muthwi <peterkilungya1@gmail.com>
2025-03-05 22:38:27 +0300

    done with variables and data types

---
+++ index.js
@@ -0,0 +1,86 @@
+// data types in javascript 
+// there are seven data types
+// 1. Number
+// 2. String  has a pair of quotation characters
+// 3. Boolean true or false
+// 4. Undefined or undefined value
+// 5. Null or empty value
+// 6. Object there are written in curly brackets {}
+// 7. Array there are written in square brackets []
+// 8. BigInt
+
+// the most important data type is object 
+// objects are written in curly brackets {}
+// objects are key value pairs
+
+// example of object
+
+
+// string are also important data type
+// strings are written in quotation marks
+
+
+//aray are also important data types
+// arrays are written in square brackets []
+// arrays are used to store multiple values in a single variable
+
+// example of array
+
+// variable 
+// variables are used to store data values
+// variables are declared with the var keyword
+// variables are assigned with the = sign
+
+// there are three was to declare variables
+// 1. var
+// 2. let
+// 3. const
+
+// var
+// var is used to declare variables
+// var is function scoped
+// var can be redeclared
+// var can be updated
+
+// let
+// let is used to declare variables
+// let is block scoped
+// let can not be redeclared
+// let can be updated
+
+// const
+// const is used to declare variables
+// const is block scoped
+// const can not be redeclared
+// const can not be updated
+
+
+// example of variables
+// var
+// let
+// const
+
+// var declaration
+
+var myVariable = "Hello World";
+myVariable += "!";
+console.log(myVariable);
+
+// let declaration
+
+let myLetVariable = "Hello World";
+myLetVariable += "!";
+console.log(myLetVariable);
+
+// const declaration
+
+const myConstVariable = "Hello World";
+myConstVariable += "!";
+console.log(myConstVariable);
+
+// data type conversion
+
+const name = julian
+
+console.log(typeof name); // string
+
